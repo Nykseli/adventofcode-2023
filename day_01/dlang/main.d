a9802f5b2f89a43d5d6756ace01485234031757f
Miika Alikirri <nykseli@protonmail.com>
2023-12-02 17:51:56 +0200

    day_01/dlang: part 2

--- day_01/dlang/main.d
+++ day_01/dlang/main.d
@@ -5,7 +5,7 @@
 +/
 import std.stdio : writefln;
 import std.file : readText;
-import std.string : strip, splitLines;
+import std.string : strip, splitLines, startsWith;
 import std.uni : isNumber;
 import std.conv : to;
 
@@ -38,6 +38,58 @@ int numInLine(string line) {
     }
 }
 
+int numInLine2(string line) {
+    int idx = 0;
+    int first = -1;
+    int last = -1;
+
+    for (; idx < line.length; idx++) {
+        if (line[idx].isNumber()) {
+            if (first == -1) {
+                first = to!int(line[idx]) - 48;
+            } else {
+                last = to!int(line[idx]) - 48;
+            }
+        }
+
+        string sub = line[idx..$];
+        int num = 0;
+        if (sub.startsWith("one")) {
+            num = 1;
+        } else if (sub.startsWith("two")) {
+            num = 2;
+        } else if (sub.startsWith("three")) {
+            num = 3;
+        } else if (sub.startsWith("four")) {
+            num = 4;
+        } else if (sub.startsWith("five")) {
+            num = 5;
+        } else if (sub.startsWith("six")) {
+            num = 6;
+        } else if (sub.startsWith("seven")) {
+            num = 7;
+        } else if (sub.startsWith("eight")) {
+            num = 8;
+        } else if (sub.startsWith("nine")) {
+            num = 9;
+        } else {
+            continue;
+        }
+
+        if (first == -1) {
+            first = num;
+        } else {
+            last = num;
+        }
+    }
+
+    if (last != -1) {
+        return (first * 10) + last;
+    } else {
+        return (first * 10) + first;
+    }
+}
+
 void part1() {
     // string[] lines = readLines("../example1.txt");
     string[] lines = readLines("../input.txt");
@@ -51,7 +103,21 @@ void part1() {
     writefln("Part 1: %d", total);
 }
 
+void part2() {
+    // string[] lines = readLines("../example2.txt");
+    string[] lines = readLines("../input.txt");
+    int total = 0;
+
+    foreach(string line; lines) {
+        int lineVal = numInLine2(line);
+        total += lineVal;
+    }
+
+    writefln("Part 2: %d", total);
+}
+
 void main()
 {
     part1();
+    part2();
 }
